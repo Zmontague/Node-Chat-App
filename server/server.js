c517a2419677628451941bb49b357c117c3341f9
Zmontague <zacharyrmontague@gmail.com>
2018-06-14 12:33:02 -0500

    Heroku Bug Fixing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../Public');
 const port = process.env.PORT || 3000;
 
 const app = express();
